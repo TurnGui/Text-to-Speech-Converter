4c4811e433b81bb4be3409c79dd8b74f78d42b8c
TurnGui <guilhermejogos@gmail.com>
2025-07-07 12:39:47 +0100

    Added gecko rain easter egg

--- script.js
+++ script.js
@@ -18,3 +18,48 @@ document.querySelector("button").addEventListener("click", () => {
         speech.text = document.querySelector("textarea").value;
         window.speechSynthesis.speak(speech);
 });
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault(); 
+        document.querySelector(".play-button").click();
+    }
+});
+
+const textarea = document.querySelector("textarea");
+
+let geckoInterval;
+let easterEggActivated = false;
+
+textarea.addEventListener("input", () => {
+    const text = textarea.value.trim();
+
+    if (text.toLowerCase() === "i love manu" && !easterEggActivated || text.toLowerCase() === "gecko rain" && !easterEggActivated) {
+        easterEggActivated = true;
+        startGeckoRain();
+    } else if (text.toLowerCase() !== "i love manu" && easterEggActivated || text.toLowerCase() === "gecko rain" && !easterEggActivated) {
+        easterEggActivated = false;
+        stopGeckoRain();
+    }
+});
+
+
+function startGeckoRain() {
+    geckoInterval = setInterval(() => {
+        const gecko = document.createElement("div");
+        gecko.classList.add("gecko-egg");
+
+        gecko.style.top = `${Math.random() * 90}vh`;
+        gecko.style.left = `${Math.random() * 90}vw`;
+
+        document.body.appendChild(gecko);
+
+        setTimeout(() => {
+            gecko.remove();
+        }, 4000);
+    }, 300); 
+}
+
+function stopGeckoRain() {
+    clearInterval(geckoInterval);
+}
